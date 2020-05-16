a70f9547ab3cfa436bb02622049896b024ecc281
Priyansi <priyanhiman@gmail.com>
2020-05-16 08:49:16 +0530

    added infinite grid

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -2,6 +2,7 @@ import random
 import itertools
 ALL_NEIGHBOURS = [
     neighbour for neighbour in itertools.product([-1, 0, 1], repeat=2) if not neighbour == (0, 0)]
+DEAD = '0'
 BLACK = 'B'
 
 
@@ -13,18 +14,37 @@ def add_blacks_around(grid):
     return new_grid
 
 
-def create_grid(rows, columns):
-    return add_blacks_around([''.join(str(random.randint(0, 1)) for column in range(columns)) for row in range(rows)])
+def add_dead_cells_around(grid):
+    return [row.replace('B', '0') for row in grid]
 
 
-def update_grid(grid, rows, columns):
+def find_random_value():
+    return 1 if random.randint(1, 100) > 75 else 0
+
+
+def create_grid(rows):
+    return add_blacks_around([''.join(str(find_random_value()) for column in range(rows)) for row in range(rows)])
+
+
+def is_cell_near_edge(grid, rows):
+    if grid[1].find('1') != -1 or grid[-2].find('1') != -1:
+        return True
+    for row in grid[1:-1]:
+        if row[0] == '1' or row[-1] == '1':
+            return True
+    return False
+
+
+def update_grid(grid, rows):
     def count_living_neighbours(row, column):
         return len([1 for row_neighbour, column_neighbour in ALL_NEIGHBOURS if grid[row + row_neighbour][column + column_neighbour] == '1'])
 
+    if is_cell_near_edge(grid, rows):
+        grid = add_blacks_around(add_dead_cells_around(grid))
+        rows += 2
     new_grid = grid.copy()
-
     for row in range(1, rows+1):
-        for column in range(1, columns+1):
+        for column in range(1, rows+1):
             living_neighbours = count_living_neighbours(row, column)
             if living_neighbours in (2, 3):
                 if living_neighbours == 3 and grid[row][column] == '0':
@@ -33,15 +53,23 @@ def update_grid(grid, rows, columns):
             else:
                 new_grid[row] = new_grid[row][:column] + \
                     '0'+new_grid[row][column+1:]
-    return new_grid
+    return new_grid, rows
 
 
-def main_game(rows, columns, CYCLES=5):
-    grid = create_grid(rows, columns)
+def game(rows, CYCLES=1):
+    all_generations = []
+    grid = create_grid(rows)
+    all_generations.append(grid)
     for cycle in range(CYCLES):
-        grid = update_grid(grid, rows, columns)
-    return grid
+        grid, rows = update_grid(grid, rows)
+        all_generations.append(grid)
+    return all_generations
 
 
 if __name__ == "__main__":
-    print(main_game(3, 2))
+    for generation in game(4, 6):
+        for row in generation:
+            for column in row:
+                print(column, end=' ')
+            print()
+        print()
